43c9161e5dec2ecee06ca5efa8e1ec3ed3c83284
hoanhtin1703 <hatin.20it6@vku.udn.vn>
2024-07-27 16:01:04 +0700

    update cấu trúc code

--- main.py
+++ main.py
@@ -2,10 +2,29 @@ import pandas as pd
 from json import loads, dumps
 from typing import Union
 from fastapi import FastAPI,status,HTTPException
+
+# ??  Cấu trúc code 
+# TODO: Khởi tạo DataFrame 
+#  ** Tạo đường dẫn truy xuất file
+#  ** Đọc dữ liệu từ file excel và trả về DataFrame
+# TODO: TODO: Xây dựng hàm để lọc dữ liệu từ DataFame
+#  ** Logic code : Sử dụng các hàm thư viện để lọc từ pandas 
+#  ** Trả về là một DataFrame đã xử lý xong logic
+# TODO : Khởi tạo Request từ Fast API 
+# ** Xây dựng các Request  ( GET,POST,PUT,DELETE)
+# ** Xử lý các ngoại lệ 
+# ** Response là một dictionary của DataFrame 
+
+
+
+# TODO: Khởi tạo DataFrame 
 def data():
+    # ** Tạo đường dẫn truy xuất file
     path = 'Lớp học phần-HK2-Năm học_ 2023-2024.xlsx'
+    # ** Đọc dữ liệu từ file excel và trả về DataFrame
     df = pd.read_excel(path)
     return df
+# TODO: Xây dựng hàm để lọc dữ liệu từ DataFame
 def list_danh_muc_hoc_phan(df):
     df = df[['Lớp học phần']]
     df.rename(columns={'Lớp học phần': 'Học phần'}, inplace=True)
@@ -15,13 +34,16 @@ def list_danh_muc_hoc_phan(df):
     df.reset_index(drop=True, inplace=True)
     return df
 app = FastAPI()
+# ** Xây dựng các Request  ( GET,POST,PUT,DELETE)
 @app.get("/")
 async def read_root():
     return "hello - Fast APi"
 @app.get("/danh-muc-hoc-phan")
 async def read_danh_muc_hoc_phan():
+    #  ** Xử lý các ngoại lệ
     try:
         clean_data = list_danh_muc_hoc_phan(data())
+        # ** Response là một dictionary của DataFrame
         return {
             "data": clean_data.to_dict(orient="records"),
             "total": len(clean_data),

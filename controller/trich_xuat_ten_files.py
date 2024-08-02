516807bb728f69df24d028adf756e4aeb8d25f22
Phuc-Tang <ttphuc.20it6@vku.udn.vn>
2024-08-02 19:08:38 +0700

    Import file excel DSSV

---
+++ controller/trich_xuat_ten_files.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import re
+
+def extract_info_from_filename(filename: str):
+    """Trích xuất số KHOA, học kỳ và năm học từ tên tệp."""
+    # Trích xuất số KHOA
+    khoa_match = re.search(r'KHOA\s*(\d+)', filename, re.IGNORECASE)
+    khoa_number = khoa_match.group(1) if khoa_match else None
+    
+    # Trích xuất học kỳ
+    hki_match = re.search(r'HKI', filename, re.IGNORECASE)
+    hkii_match = re.search(r'HKII', filename, re.IGNORECASE)
+    semester = "hk1" if hki_match else "hk2" if hkii_match else None
+    
+    # Trích xuất năm học
+    year_match = re.search(r'(\d{4})[-\s]*(\d{4})', filename)
+    if year_match:
+        start_year = year_match.group(1)[2:]
+        end_year = year_match.group(2)[2:]
+        school_year = f"{start_year}-{end_year}"
+    else:
+        school_year = None
+    
+    return khoa_number, semester, school_year
--- dataframe/data.py
+++ dataframe/data.py
@@ -1,5 +1,9 @@
 import pandas as pd
-def load_data():
-    path = "./dataframe/DS SV KHOA 21_HKII NH 2023-2024.xlsx"
-    df = pd.read_excel(path)
+# def load_data():
+#     path = "./dataframe/DS SV KHOA 21_HKII NH 2023-2024.xlsx"
+#     df = pd.read_excel(path)
+#     return df
+
+def load_data(file_path):
+    df = pd.read_excel(file_path)
     return df
